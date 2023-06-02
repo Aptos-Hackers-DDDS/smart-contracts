0adb048a49d3cc7ef10bfe85896463ca3a2f0c63
arjanjohan <arjanvanhoogdalem@gmail.com>
2023-06-02 13:43:50 +0200

    fetching data from chain

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import { Layout, Row, Col } from "antd";
+import { useState, useEffect } from "react";
+import { Layout, Row, Col, Button } from "antd";
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+import { Provider, Network } from "aptos";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+
+const provider = new Provider(Network.DEVNET);
 
 function App() {
+  const { account } = useWallet();
+  useEffect(() => {
+    fetchList();
+  }, [account?.address]);
+  const [accountHasList, setAccountHasList] = useState<boolean>(false);
+
+  const fetchList = async () => {
+    if (!account) return [];
+    // change this to be your module account address
+    const moduleAddress = "0x2b3a3c5d2118343dbda316b94fe93c36a4a4130d9084ae24366b398c7774b18d";
+    try {
+      const TodoListResource = await provider.getAccountResource(
+        account.address,
+        `${moduleAddress}::todolist::TodoList`
+      );
+      setAccountHasList(true);
+    } catch (e: any) {
+      setAccountHasList(false);
+    }
+  };
+
   return (
     <>
       <Layout>
@@ -16,6 +41,15 @@ function App() {
           </Col>
         </Row>
       </Layout>
+      {!accountHasList && (
+        <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+          <Col span={8} offset={8}>
+            <Button block type="primary" style={{ height: "40px", backgroundColor: "#3f67ff" }}>
+              Add new list
+            </Button>
+          </Col>
+        </Row>
+      )}
     </>
   );
 }
